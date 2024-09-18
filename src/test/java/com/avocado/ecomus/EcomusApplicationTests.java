cc3a54ba1f9f2d0eac0cf98800acfe582946d27e
Tan Thinh Hua <huatanthinh1207@gmail.com>
2024-09-18 22:02:41 +0900

    add project

---
+++ src/test/java/com/avocado/ecomus/EcomusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocado.ecomus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
